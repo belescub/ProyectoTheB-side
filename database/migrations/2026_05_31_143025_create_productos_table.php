47038bc0462ac151c698f644125f25b00526e6b6
IngridG-dev <mewgulfiloveyou@gmail.com>
2026-05-31 21:23:27 -0300

    Comentarios en las migrations de productos y categorias

--- database/migrations/2026_05_31_143025_create_productos_table.php
+++ database/migrations/2026_05_31_143025_create_productos_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) { //Le dice a laravel que cree una tabla nueva
             $table->id(); //es una clave primaria: es unica para cada producto
-            $table->string('nombre'); //textos cortos: VARCHAR(255) en la base
+            $table->string('nombre'); //textos cortos: VARCHAR(255) en la base. 
             $table->text('descripcion')->nullable(); //El `nullable()` permite que ese campo quede vacio.
             $table->decimal('precio', 8, 2);      // Numeros decimales, 8 digitos en total, 2 de ellos son los decimales. hasta 99999
             $table->unsignedInteger('stock')->default(0);
             $table->boolean('activo')->default(true); //Por defecto los productor nacen activos(true)
             $table->string('url_imagen')->nullable(); //Ruta de la imagen. Puede quedar vacia si el prooducto aun no tiene foto
             $table->foreignId('categoria_id')->constrained('categorias');//CLAVE FORANEA: Conecta este producto con un id que debe existir en la tabla de categoria
-            $table->timestamps(); //Crea automaticamente las columnas "created_at" y "update_at". Los timestamps siempre se dejan al final de todo, si se quiere agregar atributos nuevos. 
-            $table->softDeletes();//Crea la columna delete_at
+            $table->timestamps(); //crea automaticamente las columnas "created_at" y "update_at". Los timestamps siempre se dejan al final de todo, si se quiere agregar atributos nuevos. 
+            $table->softDeletes();//crea la columna delete_at
         });
     }
 
